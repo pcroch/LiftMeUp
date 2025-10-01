788b3f1830132a051ee159d0ab79d6d2cfa61875
pierre <pierrecrochet03@gmail.com>
2025-10-01 16:20:51 +0200

    elvetaor buidler class

---
+++ src/main/java/api/liftMeUp/model/Elevator.java
@@ -0,0 +1,53 @@
+package api.liftMeUp.model;
+
+
+import api.liftMeUp.commun.constants.Direction;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public final class Elevator {
+    private String id;
+    private int currentFloor;
+    private Direction direction;
+    private int capacity;
+    private int maxCapacity;
+
+    private Elevator(Builder builder) {
+        this.id = builder.id;
+        this.currentFloor = builder.currentFloor;
+        this.direction = builder.direction;
+        this.capacity = builder.capacity;
+        this.maxCapacity = builder.maxCapacity;
+    }
+
+    public static class Builder {
+        private String id;
+        private int currentFloor = 0;
+        private Direction direction = Direction.STATIONARY;
+        private int capacity = 0;
+        private int maxCapacity = 2;
+
+        public Builder(String id) {
+            if (id == null || id.trim().isEmpty()) {
+                throw new IllegalArgumentException("Elevator ID must not be null or empty.");
+            }
+            this.id = id;
+        }
+
+        public Builder currentFloor(int currentFloor) {
+            this.currentFloor = currentFloor;
+            return this;
+        }
+
+        public Builder capacity(int capacity) {
+            this.capacity = capacity;
+            return this;
+        }
+
+        public Elevator build() {
+            return new Elevator(this);
+        }
+    }
+    //todo builder for elevators?
+}
